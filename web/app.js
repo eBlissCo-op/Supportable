75bf5a1d88a11eca63c76209f11c787a986939db
eBlissCo-op <blissemtiness@gmail.com>
2025-11-03 02:42:25 -0500

    feat: add React app.js for web client
    Add initial React component and render call for the Supportable web client.

---
+++ web/app.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Supportable Web Client</h1>
+      <p>Welcome to the Supportable web client.</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
